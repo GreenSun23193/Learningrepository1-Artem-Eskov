2481b9cb52c928656f8d05b79d94901ba2d0e10d
NewUser324921 <89944063+NewUser324921@users.noreply.github.com>
2022-12-16 00:32:47 +0200

    Update app.js

--- server-one/app.js
+++ server-one/app.js
@@ -10,3 +10,12 @@ app.listen(port, () => {
   console.log()
 })
 'use strict';
+app.use(express.static('public'));
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    birthdate: '2010-12-25',
+    weight: 5,
+  };
+  res.json(cat);
+});
