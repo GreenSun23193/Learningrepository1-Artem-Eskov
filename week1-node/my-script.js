0cf596e8785c0216ef83d1291c943febc42bc574
Git User <artemes@metropolia.fi>
2022-12-15 15:40:46 +0200

    some message

---
+++ week1-node/my-script.js
@@ -0,0 +1,3 @@
+console.log("Hello world! 2");
+console.log('Hello world! 0 (I accidentally deleted the first Hello World) because I used >');
+var _ = require('lodash'); console.log(_.camelCase('Foo Bar'));
