f7b93984d9a1df9373aaf4ee53edc5ce9e3f93cd
Git User <artemes@metropolia.fi>
2022-12-16 00:35:39 +0200

    hi

--- app.js
+++
@@ -1,9 +0,0 @@
-app.use(express.static('public'));
-app.get('/catinfo', (req, res) => {
-  const cat = {
-    name: 'Frank',
-    birthdate: '2010-12-25',
-    weight: 5,
-  };
-  res.json(cat);
-});
