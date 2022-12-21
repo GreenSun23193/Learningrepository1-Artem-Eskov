9bed5ebc27777dea8ed8bd2c6cf263abcb8cdfe0
Git User <artemes@metropolia.fi>
2022-12-21 21:21:33 +0200

    hi

--- server-one/app.js
+++ server-one/app.js
@@ -1,15 +1,16 @@
+'use strict';
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! A')
 })
 
 app.listen(port, () => {
   console.log()
 })
-'use strict';
+
 app.use(express.static('public'));
 app.get('/catinfo', (req, res) => {
   const cat = {
@@ -20,10 +21,10 @@ app.get('/catinfo', (req, res) => {
   res.json(cat);
 });
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Title', heading: 'Click on the cat', name: 'Name', age: 'Age: 7', weight: 'Weight 5kg' })
-});
+app.get('/qqq', (req, res) => {
+  res.render('index.pug', { title: 'Title', heading: 'Click on the cat', name: 'Name', age: 'Age: 7', weight: 'Weight 5kg' })
+})
 
 console.log(`server run at http://localhost:3000`)
